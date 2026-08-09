1d9efc43c63a9f4e38de8fa47fc98eea0f0fc8ae
Desmond141 <jonasrenierfernandezloaiza@gmail.com>
2026-08-09 15:44:38 -0400

    fix: corrección wrapper

--- api/[[...slug]].js
+++ api/[[...slug]].js
@@ -1,3 +1,8 @@
 const app = require('./index');
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.url && req.url.startsWith('/api')) {
+    req.url = req.url.slice(4) || '/';
+  }
+  return app(req, res);
+};

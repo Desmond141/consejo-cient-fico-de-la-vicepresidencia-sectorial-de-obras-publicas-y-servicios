ed1eda08e51b8a488db0927bc6d092c4266291b6
Desmond141 <jonasrenierfernandezloaiza@gmail.com>
2026-08-09 16:43:01 -0400

    Fix: enrutamiendo del dominio con la pagina

--- api/[[...slug]].js
+++ api/[[...slug]].js
@@ -1,5 +1,8 @@
 const app = require('./index');
 
 module.exports = (req, res) => {
+  if (req.url && req.url.startsWith('/api')) {
+    req.url = req.url.slice(4) || '/';
+  }
   return app(req, res);
 };
